0e9ffb36fc5d7e54976763e6ae6649251aede2da
Rongji He <rhe25@uwo.ca>
2020-05-07 08:18:34 -0400

    initial commit
    add ClientHandler and WebServer

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,5 +1,39 @@
-public class ClientHandler implements Runnable{
-    public void run() {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
+public class ClientHandler implements Runnable{
+
+    private Socket socket;
+
+    public ClientHandler(Socket socket){
+        this.socket =socket;
+    }
+
+    /**
+     * HTTP server host routines:
+     * 1. Interpret request from client
+     * 2. Handle the request
+     * 3. Send response to client
+     */
+
+    public void run() {
+        try {
+            InputStream is = socket.getInputStream();
+
+            StringBuilder sb = new StringBuilder();
+            int d = -1;
+            char c1 = 'a', c2;
+            while((d=is.read())!=-1){
+                c2 = (char)d;
+                if(c2 == 10 && c1 ==13){
+                    break;
+                }
+                sb.append(c2);
+                c1=c2;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/WebServer.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    private ServerSocket server;
+
+    public WebServer(){
+        try {
+            System.out.println("Initializing server...");
+            server = new ServerSocket(7007);
+            System.out.println("Server initialization complete...");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void start(){
+        System.out.println();
+        try {
+            System.out.println("Server is listening...");
+            Socket socket = server.accept();
+            System.out.println("One client has connected!");
+            ClientHandler ch = new ClientHandler(socket);
+            Thread thread = new Thread(ch);
+            thread.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        WebServer server = new WebServer();
+        server.start();
+    }
+
+
+}
