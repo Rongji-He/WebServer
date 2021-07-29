e102ba22c9bb179641fb85d082ba7782df3a3cc4
Rongji He <rhe25@uwo.ca>
2021-07-28 21:06:05 -0400

    new service are added and read form servlet.xml

--- src/main/WebServer/core/ServletContext.java
+++ src/main/WebServer/core/ServletContext.java
@@ -3,16 +3,18 @@ package core;
 /*
 *
 * A class to store all servlet obj
-* New service and its servlet are added here
 *
 *
 * */
-import Servlet.ChangePwdServlet;
 import Servlet.HttpServlet;
-import Servlet.LoginServlet;
-import Servlet.RegServlet;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ServletContext {
@@ -24,10 +26,26 @@ public class ServletContext {
     }
 
     private static void initServletMap() {
+        SAXReader reader = new SAXReader();
+        try {
+            Document doc = reader.read(new File("./conf/servlet.xml"));
+            Element root= doc.getRootElement();
+            List<Element> list = root.elements("servlet");
+            for(Element e:list){
+                String path= e.attributeValue("path");
+                String className= e.attributeValue("className");
+                Class<?> cls= Class.forName(className);
+                HttpServlet servlet= (HttpServlet)cls.newInstance();
+                servletMap.put(path, servlet);
+            }
+        } catch (DocumentException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
         //new service and its servlet are added here
-        servletMap.put("/myweb/reg",new RegServlet());
+        /*servletMap.put("/myweb/reg",new RegServlet());
         servletMap.put("/myweb/login",new LoginServlet());
-        servletMap.put("/myweb/change",new ChangePwdServlet());
+        servletMap.put("/myweb/change",new ChangePwdServlet());*/
     }
 
     public static HttpServlet getServlet(String path){
